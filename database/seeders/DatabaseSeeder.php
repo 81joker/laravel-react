c8d83436cf69682e032f963441ea47105bd82321
Nehad <35563834+81joker@users.noreply.github.com>
2025-03-28 21:09:15 +0100

    make project model && factory  tasks model && factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,11 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Tim Altimimi',
+            'email' => 'tim26618@gmail.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
         ]);
-        $this->call([
-            PostSeeder::class,
-        ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
+        // $this->call([
+        //     PostSeeder::class,
+        // ]);
     }
 }
